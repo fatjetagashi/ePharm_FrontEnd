7996cb13fa174b6abb9e2737d27193814a1159d4
Gerta Hodolli <147100017+gertahodolli@users.noreply.github.com>
2025-05-23 02:53:45 +0200

    Add verify patient page (#53)
    * feat(layout): update sidebars & top navbars for all roles; adjust AuthContext and AdminView index
    
    * Doctor, Patient, Pharmacy APIs integrated with dynamic user flow
    
    * feat(views): add Doctor, Patient, and Pharmacy register pages
    
    * feat(PatientView): add VerifyPatientPage component

--- epharm-frontend/src/App.tsx
+++ epharm-frontend/src/App.tsx
@@ -61,102 +61,101 @@ import VerifyPatientPage from "./pages/PatientView/VerifyPatientPage";
 
 const queryClient = new QueryClient();
 const App = () => {
-    const queryClient = new QueryClient();
-    const { role } = useAuth(); // ✅ Valid usage of hook inside function
-    console.log("👤 Current role in App:", role);
-    {console.log(role)}
+  const queryClient = new QueryClient();
+  const { role } = useAuth(); // ✅ Valid usage of hook inside function
+  console.log("👤 Current role in App:", role);
 
-    {console.log(role)}
+  {console.log(role)}
 
-    return (
-        <QueryClientProvider client={queryClient}>
-            <TooltipProvider>
-                <Toaster />
-                <Sonner />
-                <BrowserRouter>
-                    <Routes>
-                        {role == null && (
-                            <>
-                                <Route path="/" element={<HomePage />} />
-                            </>
-                        )}
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/register/patient" element={<PatientRegisterPage />} />
-                        <Route path="/verify-patient-otp/:id" element={<VerifyPatientPage />} />
-                        <Route path="/register/doctor/basic" element={<DoctorRegisterPage />} />
-                        <Route path="/register/pharmacy" element={<PharmacyRegisterPage />} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            {role == null && (
+              <>
+                <Route path="/" element={<HomePage />} />
+              </>
+            )}
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register/patient" element={<PatientRegisterPage />} />
+            <Route path="/verify-patient-otp/:id" element={<VerifyPatientPage />} />
+            <Route path="/register/doctor/basic" element={<DoctorRegisterPage />} />
+            <Route path="/register/pharmacy" element={<PharmacyRegisterPage />} />
 
-                        {/* Admin View */}
-                        {role === "admin" && (
-                            <>
-                                <Route path="/" element={<AdminIndex />} />
-                                <Route path="/prescriptions" element={<AdminPrescriptionsPage />} />
-                                <Route path="/pharmacies" element={<AdminPharmaciesPage />} />
-                                <Route path="/doctors" element={<AdminDoctorsPage />} />
-                                <Route path="/patients" element={<AdminPatientsPage />} />
-                                <Route path="/medicines" element={<AdminMedicinesPage />} />
-                                <Route path="/notifications" element={<AdminNotificationsPage />} />
-                                <Route path="/settings" element={<AdminSettingsPage />} />
-                                <Route path="/reports" element={<AdminReportsPage />} />
-                                <Route path="/profile" element={<AdminProfilePage />} />
-                                <Route path="*" element={<AdminNotFound />} />
-                            </>
-                        )}
+            {/* Admin View */}
+            {role === "admin" && (
+              <>
+              <Route path="/" element={<AdminIndex />} />
+                <Route path="/prescriptions" element={<AdminPrescriptionsPage />} />
+                <Route path="/pharmacies" element={<AdminPharmaciesPage />} />
+                <Route path="/doctors" element={<AdminDoctorsPage />} />
+                <Route path="/patients" element={<AdminPatientsPage />} />
+                <Route path="/medicines" element={<AdminMedicinesPage />} />
+                <Route path="/notifications" element={<AdminNotificationsPage />} />
+                <Route path="/settings" element={<AdminSettingsPage />} />
+                <Route path="/reports" element={<AdminReportsPage />} />
+                <Route path="/profile" element={<AdminProfilePage />} />
+                <Route path="*" element={<AdminNotFound />} />
+              </>
+            )}
 
-                        {/* Patient View */}
-                        {role === "patient" && (
-                            <>
-                                <Route path="/" element={<PatientIndex />} />
-                                <Route path="/prescriptions" element={<PatientPrescriptionsPage />} />
-                                <Route path="/pharmacies" element={<PatientPharmaciesPage />} />
-                                <Route path="/doctors" element={<PatientDoctorsPage />} />
-                                <Route path="/patients" element={<PatientPatientsPage />} />
-                                <Route path="/medicines" element={<PatientMedicinesPage />} />
-                                <Route path="/notifications" element={<PatientNotificationsPage />} />
-                                <Route path="/settings" element={<PatientSettingsPage />} />
-                                <Route path="*" element={<PatientNotFound />} />
-                            </>
-                        )}
+            {/* Patient View */}
+            {role === "patient" && (
+              <>
+                <Route path="/" element={<PatientIndex />} />
+                <Route path="/prescriptions" element={<PatientPrescriptionsPage />} />
+                <Route path="/pharmacies" element={<PatientPharmaciesPage />} />
+                <Route path="/doctors" element={<PatientDoctorsPage />} />
+                <Route path="/patients" element={<PatientPatientsPage />} />
+                <Route path="/medicines" element={<PatientMedicinesPage />} />
+                <Route path="/notifications" element={<PatientNotificationsPage />} />
+                <Route path="/settings" element={<PatientSettingsPage />} />
+                <Route path="*" element={<PatientNotFound />} />
+              </>
+            )}
 
-                        {/* Doctor View */}
-                        {role === "doctor" && (
-                            <>
-                                <Route path="/" element={<DoctorIndex />} />
-                                <Route path="/appointments" element={<DoctorAppointmentsPage />} />
-                                <Route path="/create-prescription" element={<DoctorCreatePrescriptionPage />} />
-                                <Route path="/doctors" element={<DoctorDoctorsPage />} />
-                                <Route path="/patients" element={<DoctorPatientsPage />} />
-                                <Route path="/medicines" element={<DoctorMedicinesPage />} />
-                                <Route path="/notifications" element={<DoctorNotificationsPage />} />
-                                <Route path="/settings" element={<DoctorSettingsPage />} />
-                                <Route path="/register/doctor/license" element={<DoctorRegisterLicense />} />
-                                <Route path="*" element={<DoctorNotFound />} />
-                            </>
-                        )}
+            {/* Doctor View */}
+            {role === "doctor" && (
+              <>
+              <Route path="/" element={<DoctorIndex />} />
+                <Route path="/appointments" element={<DoctorAppointmentsPage />} />
+                <Route path="/create-prescription" element={<DoctorCreatePrescriptionPage />} />
+                <Route path="/doctors" element={<DoctorDoctorsPage />} />
+                <Route path="/patients" element={<DoctorPatientsPage />} />
+                <Route path="/medicines" element={<DoctorMedicinesPage />} />
+                <Route path="/notifications" element={<DoctorNotificationsPage />} />
+                <Route path="/settings" element={<DoctorSettingsPage />} />
+                <Route path="/register/doctor/license" element={<DoctorRegisterLicense />} />
+                <Route path="*" element={<DoctorNotFound />} />
+              </>
+            )}
 
-                        {
-                            role == 'pharmacy_owner' && (
-                                <>
-                                    <Route path="/" element={<Index />} />
-                                    <Route path="/pharmacy-dashboard" element={<PharmacyDashboardPage />} />
-                                    <Route path="/prescriptions" element={<PrescriptionsPage />} />
-                                    <Route path="/pharmacies" element={<PharmaciesPage />} />
-                                    <Route path="/doctors" element={<DoctorsPage />} />
-                                    <Route path="/patients" element={<PatientsPage />} />
-                                    <Route path="/medicines" element={<MedicinesPage />} />
-                                    <Route path="/orders" element={<OrdersPage />} />
-                                    <Route path="/billing" element={<BillingPage />} />
-                                    <Route path="/notifications" element={<NotificationsPage />} />
-                                    <Route path="/settings" element={<SettingsPage />} />
-                                    <Route path="*" element={<NotFound />} />
-                                </>
-                            )
-                        }
-                    </Routes>
-                </BrowserRouter>
-            </TooltipProvider>
-        </QueryClientProvider>
-    );
+            {
+              role == 'pharmacy_owner' && (
+                <>
+                <Route path="/" element={<Index />} />
+                <Route path="/pharmacy-dashboard" element={<PharmacyDashboardPage />} />
+                <Route path="/prescriptions" element={<PrescriptionsPage />} />
+                <Route path="/pharmacies" element={<PharmaciesPage />} />
+                <Route path="/doctors" element={<DoctorsPage />} />
+                <Route path="/patients" element={<PatientsPage />} />
+                <Route path="/medicines" element={<MedicinesPage />} />
+                <Route path="/orders" element={<OrdersPage />} />
+                <Route path="/billing" element={<BillingPage />} />
+                <Route path="/notifications" element={<NotificationsPage />} />
+                <Route path="/settings" element={<SettingsPage />} />
+                <Route path="*" element={<NotFound />} />
+                </>
+              )
+            }
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
